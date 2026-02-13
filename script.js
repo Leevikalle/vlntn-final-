ee095bc27165518e0ce7ea47e18c8bd3784a2a58
Leevikalle <93085986+Leevikalle@users.noreply.github.com>
2026-02-13 21:51:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,29 @@
 const yesButton = document.getElementById("yes");
 const noButton = document.getElementById("no");
 const result = document.getElementById("result");
+const result1 = document.getElementById("result1");
+
+const maxX = window.innerWidth - 100;
+const maxY = window.innerHeight - 50;
+
+const img = document.getElementById("surprise");
 
 // Add functions and event listeners
 yesButton.onclick = function() {
     result.innerText = "Yayy!";
+    img.style.opacity = 1;
+    result1.innerText = "Leevi on juuri voittanut saman kuin 533 lottovoittoa."
+    result1.style.opacity = 1;
+
 };
 
+noButton.onclick = function()   {
+    moveButton();
 
-noButton.onclick = function() {
-    yesButton.innerText = "No";
-};
+}
+function moveButton()   {
+    const randomX = Math.random()*maxX;
+    const randomY = Math.random()*maxY;
+    noButton.style.left = randomX+"px";
+    noButton.style.top = randomY+"px";
+}    
