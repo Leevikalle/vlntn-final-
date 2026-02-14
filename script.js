9f8288012bc2b941e1c2cef5515ca23d42832a23
Leevikalle <93085986+Leevikalle@users.noreply.github.com>
2026-02-14 10:11:48 +0200

    Add files via upload
    Updated(2)

--- script.js
+++ script.js
@@ -9,9 +9,12 @@ const maxY = window.innerHeight - 50;
 
 const img = document.getElementById("surprise");
 
+let currentSize = parseInt(window.getComputedStyle(yesButton).fontSize);
+
 // Add functions and event listeners
 yesButton.onclick = function() {
     result.innerText = "Yayy!";
+    img.style.transform = "scale(1)";
     img.style.opacity = 1;
     result1.innerText = "Leevi on juuri voittanut saman kuin 533 lottovoittoa."
     result1.style.opacity = 1;
@@ -20,11 +23,18 @@ yesButton.onclick = function() {
 
 noButton.onclick = function()   {
     moveButton();
+    increaseButton();
 
-}
+
+};
 function moveButton()   {
     const randomX = Math.random()*maxX;
     const randomY = Math.random()*maxY;
     noButton.style.left = randomX+"px";
     noButton.style.top = randomY+"px";
-}    
+};    
+
+function increaseButton()   {
+    currentSize += 5;
+    yesButton.style.fontSize = currentSize + "px";
+}
